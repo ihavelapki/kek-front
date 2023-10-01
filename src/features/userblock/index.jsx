e05ad9cad7524c08b9d3b0c2df9a3d0c6aeb8233
Pereyro <petrgubko@gmail.com>
2023-10-01 20:36:37 +0300

    did points 10.2, 11.1

--- src/features/userblock/index.jsx
+++ src/features/userblock/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './Userblock.module.css';
+import KekModal from '../../shared/ui/KekModal';
 
 const Userblock = () => {
+
+    const [hidden, setHidden] = useState(false);
+    // console.log(hidden);
+
     return (
         <div className={classes.usericon}>
-
+            <button className={classes.user_btn} onClick={() => setHidden(true)}>KEK</button>
+            <KekModal visible={hidden} setVisible={setHidden}>
+                <div className={classes.user_dialog_list}>
+                    <Link to="/afisha"><p className="title">user</p></Link>
+                    <Link to="/afisha"><p className="title">kek</p></Link>
+                    <Link to="/afisha"><p className="title">help</p></Link>
+                    <Link to="/afisha"><p className="title">login</p></Link>
+                </div>
+            </KekModal>
         </div>
     );
 };
