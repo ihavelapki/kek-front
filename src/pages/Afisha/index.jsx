ebab03c7f5201fe7c69233cb7c8273630d1b1b46
Pereyro <petrgubko@gmail.com>
2023-10-17 17:28:56 +0300

    async function that returns list of films has been added

--- src/pages/Afisha/index.jsx
+++ src/pages/Afisha/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Films from "../../widgets/films";
+import { fetchFilms } from '../../shared/api';
 
 const Afisha = () => {
-    const filmList = [{'id': 1, 'title': 'One Piece', 'year': 2007, 'description': 'ajobdskbjbd', 'image': '/images/film1.jpg', 'date': '2023-09-12'},
-    {'id': 2, 'title': 'One Piece', 'year': 2007, 'description': 'ajobdskbjbd', 'image': '/images/film1.jpg', 'date': '2023-08-31'},
-]
+
+    const [filmList, setFilmList] = useState([{'id': 1, 'title': 'One Piece', 'year': 2007, 'description': 'ajobdskbjbd', 'image': '/images/film1.jpg', 'date': '2023-09-12'},
+        {'id': 2, 'title': 'One Piece', 'year': 2007, 'description': 'ajobdskbjbd', 'image': '/images/film1.jpg', 'date': '2023-08-31'}
+    ]);
+
+    async function getFilms() {
+        const films = await fetchFilms();
+        setFilmList(films);
+    }
+
+    useEffect(() => {
+        getFilms();
+    }, [])
+
     return (
         <div className="wrapper">
             <h1>THIS IS AFISHA PAGE</h1>
