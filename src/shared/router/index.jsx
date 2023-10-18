3f8d45e56832291e98284156b11936c3b18a5eea
Pereyro <petrgubko@gmail.com>
2023-10-18 21:53:21 +0300

    Private route function has been added inside router/index element

--- src/shared/router/index.jsx
+++ src/shared/router/index.jsx
@@ -5,16 +5,25 @@ import Afisha from "../../pages/Afisha";
 import FilmPage from '../../pages/FlimPage';
 import BarGrill from '../../pages/BarGrill';
 import LoginPage from '../../pages/LoginPage';
+import { useAuth } from '../../shared/auth';
 
+const PrivateRoute = ({element}) => {
+  const {isAuthentificated} = useAuth();
 
+  return isAuthentificated ? (
+    element ) : (
+      <Navigate to="/login" />
+  )
+
+}
 
 function KekRouter() {
   return ( 
       <Routes>
         <Route path="/about" element={<About/> } />
-        <Route exact path="/afisha" element={<Afisha/> } />
-        <Route exact path="/afisha/:id" element={<FilmPage/> } />
-        <Route path="/bargrill" element={<BarGrill/>} />
+        <Route exact path="/afisha" element={<PrivateRoute element={<Afisha/> } />} />
+        <Route exact path="/afisha/:id" element={<PrivateRoute element={<FilmPage/> } />} />
+        <Route path="/bargrill" element={<PrivateRoute element={<BarGrill/>} />} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="*" element={<Navigate to="/about" />} />
       </Routes> 
